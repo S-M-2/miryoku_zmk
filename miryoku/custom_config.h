a008942ba59ebc37bbcaa05f21b0243ee8fe2ed4
S-M-2 <zaamppa@hotmail.com>
2024-04-20 14:15:41 +0300

    custom-config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,7 +11,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              &lt U_TAP ESC,     &kp TAB,           &kp SPACE,         U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_TAP, ESC), &kp TAB,           &kp SPACE,         U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_TAP \
 &kp N1,            &kp N2,            &kp N3,            &kp N4,            &kp N5,            &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
