09b21ab619acbf9e77a7dd92959c279f94f5e10e
S-M-2 <zaamppa@hotmail.com>
2024-06-21 14:11:38 +0300

    tab-space

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,20 +1,27 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_LAYER_BASE \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+#define MIRYOKU_LAYER_BASE                                                     \
+  &kp Q, &kp W, &kp E, &kp R, &kp T, &kp Y, &kp U, &kp I, &kp O, &kp P,        \
+      U_MT(LGUI, A), U_MT(LALT, S), U_MT(LCTRL, D), U_MT(LSHFT, F), &kp G,     \
+      &kp H, U_MT(LSHFT, J), U_MT(LCTRL, K), U_MT(LALT, L), U_MT(LGUI, SQT),   \
+      U_LT(U_BUTTON, Z), U_MT(RALT, X), &kp C, &kp V, &kp B, &kp N, &kp M,     \
+      &kp COMMA, U_MT(RALT, DOT), U_LT(U_BUTTON, SLASH), U_NP, U_NP,           \
+      U_LT(U_MEDIA, ESC), U_LT(U_NAV, TAB), U_LT(U_MOUSE, SPACE),              \
+      U_LT(U_SYM, RET), U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL), U_NP, U_NP
 
-#define MIRYOKU_LAYER_EXTRA \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-&kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
-&kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_TAP, ESC), &kp TAB,           &kp SPACE,         U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+#define MIRYOKU_LAYER_EXTRA                                                    \
+  &kp Q, &kp W, &kp E, &kp R, &kp T, &kp Y, &kp U, &kp I, &kp O, &kp P, &kp A, \
+      &kp S, &kp D, &kp F, &kp G, &kp H, U_MT(LSHFT, J), U_MT(LCTRL, K),       \
+      U_MT(LALT, L), U_MT(LGUI, SQT), &kp Z, &kp X, &kp C, &kp V, &kp B,       \
+      &kp N, &kp M, &kp COMMA, U_MT(RALT, DOT), U_LT(U_BUTTON, SLASH), U_NP,   \
+      U_NP, U_LT(U_TAP, ESC), &kp TAB, &kp SPACE, U_LT(U_SYM, RET),            \
+      U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL), U_NP, U_NP
 
-#define MIRYOKU_LAYER_TAP \
-&kp N1,            &kp N2,            &kp N3,            &kp N4,            &kp N5,            &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-&kp A,             &kp S,             &kp D,             &kp F,             &kp N6,            &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
-&kp COMMA,         &kp N0,            &kp N9,            &kp N8,            &kp N7,            &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+#define MIRYOKU_LAYER_TAP                                                      \
+  &kp N1, &kp N2, &kp N3, &kp N4, &kp N5, &kp Y, &kp U, &kp I, &kp O, &kp P,   \
+      &kp A, &kp S, &kp D, &kp F, &kp N6, &kp H, U_MT(LSHFT, J),               \
+      U_MT(LCTRL, K), U_MT(LALT, L), U_MT(LGUI, SQT), &kp COMMA, &kp N0,       \
+      &kp N9, &kp N8, &kp N7, &kp N, &kp M, &kp COMMA, U_MT(RALT, DOT),        \
+      U_LT(U_BUTTON, SLASH), U_NP, U_NP, U_NA, U_NA, U_NA, U_LT(U_SYM, RET),   \
+      U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL), U_NP, U_NP
