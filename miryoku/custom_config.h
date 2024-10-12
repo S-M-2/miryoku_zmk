ef2d3395dfd13afe8cc78bf529722797730d398f
S-M-2 <zaamppa@hotmail.com>
2024-10-12 10:26:34 +0300

    TOTEM

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,6 +2,26 @@
 // https://github.com/manna-harbour/miryoku
 // clang-format off
 
+#if !defined (MIRYOKU_LAYOUTMAPPING_TOTEM)
+
+#define XXX &none
+
+#define MIRYOKU_LAYOUTMAPPING_TOTEM( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     K30, N31, K32, K33, K34,      K35, K36, K37, N38, K39 \
+) \
+     K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 \
+     K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
+&kp PG_UP  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp PG_UP \
+               K32  K33  K34       K35  K36  K37
+
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 33 34
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 35 36
+
+#endif
+
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
@@ -12,7 +32,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 &mt LCTRL Z,       &mt LALT X,        &mt LSHIFT C,      &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_TAP, ESC),  &kp TAB,           &kp SPACE,         U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_TAP, PG_DN),  &kp TAB,           &kp SPACE,         U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_TAP \
 &mt ESC N1,        &kp N2,            &kp N3,            &kp N4,            &kp N5,            &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
